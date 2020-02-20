a8d623cd8ac926e04037ba725112fcfa8a16410f
lowell1 <29132034+lowell1@users.noreply.github.com>
2020-02-19 23:48:49 -0800

    rock paper scissors

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,9 +2,57 @@
 
 import sys
 
-def rock_paper_scissors(n):
-  pass 
+# def recurse(n, idx):
+#   new_combos = []
 
+#   if idx == n - 1:
+#     arr = ["rock"] * n
+#     for text in ["rock", "paper", "scissors"]:
+#       new_combos.append(arr[:-1] + [text])
+
+#   else:
+#     print(idx)
+#     last_combos = recurse(n, idx + 1)
+
+#     for combo in last_combos:
+#       for text in ["rock", "paper", "scissors"]:
+#         new_combos.append(combo[:idx] + [text] + combo[idx + 1:])
+
+#   return new_combos
+
+def recurse(n, idx):
+  new_combos = []
+
+  if idx == n - 1:
+    arr = ["rock"] * n
+    for text in ["rock", "paper", "scissors"]:
+      new_combos.append(arr[:-1] + [text])
+  else:
+    last_combos = recurse(n, idx + 1)
+    for text in ["rock", "paper", "scissors"]:
+      for combo in last_combos:
+        new_combos.append(combo[:idx] + [text] + combo[idx + 1:])
+
+  return new_combos
+
+def rock_paper_scissors(n, idx = 0):
+  if(n == 0):
+    return [[]]
+
+  new_combos = []
+
+  if idx == n - 1:
+    arr = ["rock"] * n
+    for text in ["rock", "paper", "scissors"]:
+      new_combos.append(arr[:-1] + [text])
+  else:
+    last_combos = rock_paper_scissors(n, idx + 1)
+    for text in ["rock", "paper", "scissors"]:
+      for combo in last_combos:
+        new_combos.append(combo[:idx] + [text] + combo[idx + 1:])
+
+  return new_combos
+  
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
